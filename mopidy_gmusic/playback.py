e474f0aec04445b93e1d1205f1286fa71f6fd044
Felix Bechstein <f@ub0r.de>
2014-07-30 19:44:21 +0200

    Increment play counts
    Increment play counts for played tracks if 2/3 of track where played

--- mopidy_gmusic/playback.py
+++ mopidy_gmusic/playback.py
@@ -1,14 +1,40 @@
 from __future__ import unicode_literals
 
+import logging
+
+import time
+
 from mopidy import backend
+
+logger = logging.getLogger(__name__)
 
 
 class GMusicPlaybackProvider(backend.PlaybackProvider):
+    def __init__(self, audio, backend):
+        super(GMusicPlaybackProvider, self).__init__(audio, backend)
+        self._track_start = 0
+        self._track = None
 
     def play(self, track):
+        logger.debug('play(): %r', track)
         url = self.backend.session.get_stream_url(track.uri.split(':')[2])
         if url is None:
             return False
         self.audio.prepare_change()
         self.audio.set_uri(url).get()
+        self._track_start = time.time()
+        self._track = track
         return self.audio.start_playback().get()
+
+    def stop(self):
+        super(GMusicPlaybackProvider, self).stop()
+        if not self._track:
+            logger.debug('Current track is unset')
+        elif 0 < self._track.length * 2/3 \
+               < (time.time() - self._track_start) * 1000:
+            track_id = self._track.uri.split(':')[2]
+            logger.debug('Broadcast play to google music: %r', track_id)
+            self.backend.session.increment_song_playcount(track_id)
+            self._track = None  # prevent additional calls
+        else:
+            logger.debug('Track got skipped')
