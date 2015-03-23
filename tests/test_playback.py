4fe06fd92f45e799c3dd21157d676a2c1c6b5107
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-23 14:46:16 +0100

    playback: Update tests

--- tests/test_playback.py
+++ tests/test_playback.py
@@ -30,32 +30,26 @@ class PlaybackTest(unittest.TestCase):
         p = GMusicPlaybackProvider(audio, backend)
         self.assertIsNotNone(p)
 
-    def test_play_invalid(self):
+    def test_change_track_invalid(self):
         audio = mock.Mock()
-        audio.prepare_change = mock.Mock()
-        audio.set_uri = mock.Mock()
-        audio.start_playback = mock.Mock()
         backend = mock.Mock()
         backend.session = GMusicSession()
         p = GMusicPlaybackProvider(audio, backend)
-        self.assertFalse(p.play(Track(uri='gmusic:track:invalid_uri')))
+        self.assertFalse(p.change_track(Track(uri='gmusic:track:invalid_uri')))
         self.assertEqual(p.audio.prepare_change.call_count, 0)
         self.assertEqual(p.audio.set_uri.call_count, 0)
         self.assertEqual(p.audio.start_playback.call_count, 0)
 
-    def test_play_valid(self):
+    def test_change_track_valid(self):
         audio = mock.Mock()
-        audio.prepare_change = mock.Mock()
-        audio.set_uri = mock.Mock()
-        audio.start_playback = mock.Mock()
         backend = mock.Mock()
         backend.session.get_stream_url = mock.Mock(
             return_value='http://stream.example.com/foo.mp3')
         p = GMusicPlaybackProvider(audio, backend)
-        self.assertIsNotNone(p.play(Track(uri='gmusic:track:valid_uri')))
-        self.assertEqual(p.audio.prepare_change.call_count, 1)
+        self.assertTrue(p.change_track(Track(uri='gmusic:track:valid_uri')))
+        self.assertEqual(p.audio.prepare_change.call_count, 0)
         self.assertEqual(p.audio.set_uri.call_count, 1)
-        self.assertEqual(p.audio.start_playback.call_count, 1)
+        self.assertEqual(p.audio.start_playback.call_count, 0)
 
     def _setup_player(self):
         audio = mock.Mock()
@@ -67,7 +61,8 @@ class PlaybackTest(unittest.TestCase):
 
     def test_playback(self):
         playback = self._setup_player()
-        playback.play(self._track)
+        playback.change_track(self._track)
+        playback.play()
 
         playback.backend.session.get_stream_url.assert_called_once()
         playback.audio.start_playback.assert_called_once_with()
@@ -78,7 +73,8 @@ class PlaybackTest(unittest.TestCase):
 
     def test_stop_full_track(self):
         playback = self._setup_player()
-        playback.play(self._track)
+        playback.change_track(self._track)
+        playback.play()
         # sleep for track length
         time.sleep(0.8)
         playback.stop()
@@ -90,7 +86,8 @@ class PlaybackTest(unittest.TestCase):
 
     def test_stop_skip(self):
         playback = self._setup_player()
-        playback.play(self._track)
+        playback.change_track(self._track)
+        playback.play()
         # sleep for 1/3 of track length
         time.sleep(0.3)
         playback.stop()
